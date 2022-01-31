8dc03fbd9986bfa1dbfed86080dec5bd9acc33bf
Dedrie <dedrie.peart@gmail.com>
2022-01-30 23:52:21 -0500

    added image for save button and added text area when clicked

--- assets/js.js
+++ assets/js.js
@@ -1,11 +1,28 @@
 
 document.getElementById("currentDay")
 var currentDate = moment().format("dddd, MMMM Do YYYY")
-
+document.getElementsByClassName("schedule-block")
 
 console.log(currentDate)
 
 // display current time
-    $("#currentDay").text(currentDate)
+$("#currentDay").text(currentDate)
+
+//update  schedule when clicked
+$(".content").click(function () {
+    var text = $(this)
+        .text()
+        .trim();
+    var textInput = $("<textarea>")
+        .addClass("form-control")
+        .val(text);
+     $(this).replaceWith(textInput);
+
+})
+
+// save changes
+
+//color time
+
 
 
